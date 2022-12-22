09aa3e62a22dd5b7a414c24a5791a0c6a1894bb5
namanjain123 <naman.nishi@gmail.com>
2022-12-23 00:07:51 +0530

    new js and other twitter project

---
+++ JavaScript/dataType.js
@@ -0,0 +1,10 @@
+let numOne="";
+let nums=[1,2,3];
+//strings
+
+console.log(numOne);
+let string = 'Love is the most powerful feeling in the world'
+
+console.log(string.endsWith('world'))       ;  // true
+console.log(string.endsWith('love'))         ; // false
+console.log(string.endsWith('in the world'));
