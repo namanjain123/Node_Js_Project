97f024dcae0159275487dd7369668d39836ca339
namanjain123 <naman.nishi@gmail.com>
2022-12-23 22:01:33 +0530

    new files of javascript of loops etc

---
+++ JavaScript/arrayAndOthers.js
@@ -0,0 +1,40 @@
+const arr = Array()
+const arr2=[]
+let js="naman"
+const chararr=js.split('')
+// manipulate the array
+//FILL
+const eight0values = Array(8).fill(0) // it creates eight element values filled with '0'
+console.log(eight0values) // [0, 0, 0, 0, 0, 0, 0, 0]
+//concat
+const firstList = [1, 2, 3]
+const secondList = [4, 5, 6]
+const thirdList = firstList.concat(secondList)
+console.log(thirdList)
+//length
+numbers = [1, 2, 3, 4, 5]
+
+console.log(numbers.indexOf(5))
+
+//last index
+numbers = [1, 2, 3, 4, 5, 3, 1, 2]
+console.log(numbers.lastIndexOf(2))
+
+
+// check array
+numbers = [1, 2, 3, 4, 5]
+console.log(numbers.toString())
+
+//slice
+const numbers = [1,2,3,4,5]
+console.log(numbers.slice())
+
+
+
+
+
+
+
+
+
+
---
+++ JavaScript/loops.js
@@ -0,0 +1,28 @@
+//for loop
+for(let i = 5; i >= 0; i--){
+    console.log(i)
+  }
+
+  //while
+  let i = 0
+while (i <= 5) {
+  console.log(i)
+  i++
+}
+
+//do while
+i = 0
+do {
+  console.log(i)
+  i++
+} while (i <= 5)
+
+//for with content
+const numbers = [1, 2, 3, 4, 5]
+
+for (const num of numbers) {
+  console.log(num)
+}
+
+
+  
