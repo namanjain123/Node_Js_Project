23cdcbefd4e01b188bbc4cc1ba6df4f4f5c2757f
namanjain123 <naman.nishi@gmail.com>
2022-12-25 00:28:27 +0530

    addition of new object files

---
+++ JavaScript/object.js
@@ -0,0 +1,14 @@
+//window global object
+
+a = 'JavaScript'
+b = 10 
+function letsLearnScope() {
+  console.log(a, b)
+  if (true) {
+    console.log(a, b)
+  }
+}
+console.log(a, b) // accessible
+
+  
+
