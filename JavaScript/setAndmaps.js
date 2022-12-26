d6406d6ad3d7c386b0652b4b32cb840cdc59b8a9
namanjain123 <naman.nishi@gmail.com>
2022-12-26 18:57:18 +0530

    addition of new js files

---
+++ JavaScript/setAndmaps.js
@@ -0,0 +1,25 @@
+const companies = new Set()
+console.log(companies)
+
+const lang=[
+    'naman',
+    'jain',
+    'new',
+    'latest'
+]
+
+const setOf=new Set(lang)
+console.log(setOf)
+
+// Adding an element to a set
+
+setOf.add('new set')
+console.log(setOf)
+
+// Addition from a set to new set
+
+companies = ['Google', 'Facebook', 'Amazon', 'Oracle', 'Microsoft']
+for (const company of companies) {
+  setOf.add(company)
+}
+console.log(setOf)
