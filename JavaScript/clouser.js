bd79a591d430affde659beebae34d6fc09b539b0
namanjain123 <naman.nishi@gmail.com>
2022-12-29 23:20:39 +0530

    addiiton of clouser, fetch api and others

---
+++ JavaScript/clouser.js
@@ -0,0 +1,20 @@
+function outerFunction() {
+    let count = 0;
+    function plusOne() {
+        count++
+        return count
+    }
+    function minusOne() {
+        count--
+        return count
+    }
+
+    return {
+        plusOne:plusOne(),
+        minusOne:minusOne()
+    }
+}
+const innerFuncs = outerFunction()
+
+console.log(innerFuncs.plusOne)
+console.log(innerFuncs.minusOne)
---
+++ JavaScript/fetchapi.js
@@ -0,0 +1,9 @@
+
+const url = '' 
+fetch(url)
+  .then(response => response.json()) // accessing the API data as JSON
+  .then(data => {
+    // getting the data
+    console.log(data)
+  })
+  .catch(error => console.error(error))
