4b3a5ef7ddeab7ea43d7c134d0f32b26bca9c658
namanjain123 <naman.nishi@gmail.com>
2022-12-30 14:34:01 +0530

    using of Dom in js

---
+++ JavaScript/dom.js
@@ -0,0 +1,42 @@
+// Get the element by tag
+document.getElementsByTagName('tagname')
+const allTitles = document.getElementsByTagName('h1')
+
+console.log(allTitles) //HTMLCollections
+console.log(allTitles.length) // 4
+
+for (let i = 0; i < allTitles.length; i++) {
+  console.log(allTitles[i]) // prints each elements in the HTMLCollection
+}
+
+//get element by tag class name
+allTitles = document.getElementsByClassName('title')
+
+console.log(allTitles) //HTMLCollections
+console.log(allTitles.length) // 4
+
+for (let i = 0; i < allTitles.length; i++) {
+  console.log(allTitles[i]) // prints each elements in the HTMLCollection
+}
+
+// Adding Attribute
+
+const titles = document.querySelectorAll('h1')
+titles[3].setAttribute('class', 'title')
+titles[3].setAttribute('id', 'fourth-title')
+
+//Adding text to html element
+titles = document.querySelectorAll('h1')
+titles[3].textContent = 'Fourth Title'
+
+
+//Adding Style Color
+const titles = document.querySelectorAll('h1')
+titles.forEach((title, i) => {
+  title.style.fontSize = '24px' // all titles will have 24px font size
+  if (i % 2 === 0) {
+    title.style.backgroundColor = 'green'
+  } else {
+    title.style.backgroundColor = 'red'
+  }
+})
